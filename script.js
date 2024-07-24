1eb3ff09216068d982586d033374236a2b600ce1
Anas Al-rawi <rawi.anas@gmail.com>
2024-07-24 15:04:38 -0400

    Add function to randomize message elements

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const message_data = {
     // The possible blessings are in order from best to worst
-    possibleBlessings: ['Great blessing', 'Blessing', 'Small blessing', 'Half blessing', 'Future blessing', 'Future small Blessing', 'Misfortune'],
+    blessings: ['Great blessing', 'Blessing', 'Small blessing', 'Half blessing', 'Future blessing', 'Future small Blessing', 'Misfortune'],
     // There is no ordering for life aspects and their predictions
-    possibleLifeAspects: ['Wishes', 'Lost items', 'Travel', 'Business', 'School', 'Relationships', 'Marriage', 'Childbirth', 'Health', 'Disputes'],
-    possibleAspectPredictions: ['Do not be afraid of competition.', 'An exciting opportunity lies ahead of you.', 'Be true to your word.', 'Your ideas have exeptional merit.', 'Plan for many pleasures ahead.', 'It is easier to stay out than to get out.', 'Expect the unexpected.', 'Something routine will turn into an enchanting adventure.', 'Beware of any endeavors that require new clothes.', 'You will not be comfortable without your own approval.', 'You will recieve a blessing from an unexpected source.', 'Attitude is a little thing that makes a bid difference.', 'Once you make a decision the universe conspires to make it happen.', 'Nothing great will be achieved without enthusiasm.', 'Reach for joy and all else will follow.', 'There are no impossibilities.', 'It is better to take smaller steps this year.', 'Apreciate what you already have.', 'You will have better luck in the following year.', 'Do not open your door on the last day of the month.', 'Someone close to you will be blessed in this.']
-}
+    lifeAspects: ['Wishes', 'Lost items', 'Travel', 'Business', 'School', 'Relationships', 'Marriage', 'Childbirth', 'Health', 'Disputes'],
+    aspectPredictions: ['Do not be afraid of competition.', 'An exciting opportunity lies ahead of you.', 'Be true to your word.', 'Your ideas have exeptional merit.', 'Plan for many pleasures ahead.', 'It is easier to stay out than to get out.', 'Expect the unexpected.', 'Something routine will turn into an enchanting adventure.', 'Beware of any endeavors that require new clothes.', 'You will not be comfortable without your own approval.', 'You will recieve a blessing from an unexpected source.', 'Attitude is a little thing that makes a bid difference.', 'Once you make a decision the universe conspires to make it happen.', 'Nothing great will be achieved without enthusiasm.', 'Reach for joy and all else will follow.', 'There are no impossibilities.', 'It is better to take smaller steps this year.', 'Apreciate what you already have.', 'You will have better luck in the following year.', 'Do not open your door on the last day of the month.', 'Someone close to you will be blessed in this.']
+};
 
-let finalMessage = `test message`
+const selector = listName => {
+    if (listName in message_data) {
+        randomItemIndex = Math.floor(Math.random() * message_data[listName].length);
+        return message_data[listName][randomItemIndex]
+    } else {
+        return 'Invalid input'
+    }
+};
 
-console.log(message_data.possibleAspectPredictions.length)
+let finalMessage = `test message`;
+
+console.log(selector('blessings'));
